07afd816b35bb9f551256d931a53681dcbd9c2ec
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-05-10 09:28:03 -0700

    Update single-publication.php
    Removal of sidebar
    Addition of each descriptive item of a publication as (optional) right column item (Author, Journal, Volume, Pages, ISSN, DOI)and post abstract as left column
    Addition of the link URL to the publication

--- single-publication.php
+++ single-publication.php
@@ -1,4 +1,4 @@
-        <?php
+       <?php
 
 $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
 
@@ -20,7 +20,7 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
     <div class="ucla campus">
 
       <div class="col span_9_of_12"><?php edit_post_link(); ?>
-         <?php the_content(); ?>
+ <?php the_content(); ?>
 
         <?php
             /** @var string|false|WP_Error $tag_list */
@@ -34,8 +34,44 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
 
     <?php endwhile; endif; ?>
 
-    <div class="col span_3_of_12">
-      <?php get_sidebar(); ?>
+<div class="col span_3_of_12">
+<ul>
+<?php if(get_post_meta($post->ID, 'Author', true)):?>
+<li>
+ Author: <?php echo get_post_meta($post->ID, 'Author', true);?>
+</li>
+<?php endif;?>
+<?php if(get_post_meta($post->ID, 'Journal', true)):?>
+<li>
+ Journal: <?php echo get_post_meta($post->ID, 'Journal', true);?>
+</li>
+<?php endif;?>
+<?php if(get_post_meta($post->ID, 'Volume', true)):?>
+<li>
+ Volume: <?php echo get_post_meta($post->ID, 'Volume', true);?>
+</li>
+<?php endif;?>
+<?php if(get_post_meta($post->ID, 'Pages', true)):?>
+<li> 
+ Pages: <?php echo get_post_meta($post->ID, 'Pages', true);?>
+</li>
+<?php endif;?>
+<?php if(get_post_meta($post->ID, 'ISSN', true)):?>
+<li>
+ ISSN: <?php echo get_post_meta($post->ID, 'ISSN', true);?>
+</li>
+<?php endif;?>
+<?php if( get_post_meta($post->ID, 'DOI', true)):?>
+
+<li>
+ DOI: <?php echo get_post_meta($post->ID, 'DOI', true);?>
+</li>
+<?php endif;?>
+<?php if(get_post_meta($post->ID, 'URL', true)):?>
+<?php
+            echo '<li> URL: <a href="' . get_post_meta($post->ID, 'URL', true). '"> link to publication</a></li>';?>
+<?php endif;?>
+</ul> 
     </div>
 
   </div>
