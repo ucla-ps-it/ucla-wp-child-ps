7a2b89280f1ca31466e93e0596170d0e91567594
Cody Ashe-McNalley <codyam@g.ucla.edu>
2021-05-23 20:17:20 -0400

    Enabling local profile pictures

--- functions.php
+++ functions.php
@@ -32,3 +32,126 @@ function AOS_increase_upload( $bytes )
 {
 	return 26214400; // 1 megabyte
 }                // CPT 
+
+
+/**
+ * Custom Avatar (Profile Picture) Without a Plugin
+ */
+// 1. Enqueue the needed scripts.
+add_action( "admin_enqueue_scripts", "deptdir_enqueue" );
+function deptdir_enqueue( $hook ){
+	// Load scripts only on the user-edit page.
+	if( $hook === 'user-edit.php' ){
+		add_thickbox();
+		wp_enqueue_script( 'media-upload' );
+		wp_enqueue_media();
+	}
+}
+
+// 2. Scripts for Media Uploader.
+function deptdir_admin_media_scripts() {
+	?>
+	<script>
+		jQuery(document).ready(function ($) {
+			$(document).on('click', '.avatar-image-upload', function (e) {
+				e.preventDefault();
+				var $button = $(this);
+				var file_frame = wp.media.frames.file_frame = wp.media({
+					title: 'Select or Upload an Custom Avatar',
+					library: {
+						type: 'image' // mime type
+					},
+					button: {
+						text: 'Select Avatar'
+					},
+					multiple: false
+				});
+				file_frame.on('select', function() {
+					var attachment = file_frame.state().get('selection').first().toJSON();
+					$button.siblings('#deptdir-custom-avatar').val( attachment.sizes.thumbnail.url );
+					$button.siblings('.custom-avatar-preview').attr( 'src', attachment.sizes.thumbnail.url );
+				});
+				file_frame.open();
+			});
+		});
+	</script>
+	<?php
+}
+//add_action( 'admin_print_footer_scripts-profile.php', 'deptdir_admin_media_scripts' );
+add_action( 'admin_print_footer_scripts-user-edit.php', 'deptdir_admin_media_scripts' );
+
+// 3. Adding the Custom Image section for avatar.
+function custom_user_profile_fields( $profileuser ) {
+	?>
+	<h3><?php _e('Custom Local Avatar', 'deptdir'); ?></h3>
+	<table class="form-table deptdir-avatar-upload-options">
+		<tr>
+			<th>
+				<label for="image"><?php _e('Custom Local Avatar', 'deptdir'); ?></label>
+			</th>
+			<td>
+				<?php
+				// Check whether we saved the custom avatar, else return the default avatar.
+				$custom_avatar = get_the_author_meta( 'deptdir-custom-avatar', $profileuser->ID );
+				if ( $custom_avatar == '' ){
+					$custom_avatar = get_avatar_url( $profileuser->ID );
+				}else{
+					$custom_avatar = esc_url_raw( $custom_avatar );
+				}
+				?>
+				<img style="width: 96px; height: 96px; display: block; margin-bottom: 15px;" class="custom-avatar-preview" src="<?php echo $custom_avatar; ?>">
+				<input type="text" name="deptdir-custom-avatar" id="deptdir-custom-avatar" value="<?php echo esc_attr( esc_url_raw( get_the_author_meta( 'deptdir-custom-avatar', $profileuser->ID ) ) ); ?>" class="regular-text" />
+				<input type='button' class="avatar-image-upload button-primary" value="<?php esc_attr_e("Upload Image","deptdir");?>" id="uploadimage"/><br />
+				<span class="description">
+					<?php _e('Please upload a custom avatar for your profile, to remove the avatar simple delete the URL and click update.', 'deptdir'); ?>
+				</span>
+			</td>
+		</tr>
+	</table>
+	<?php
+}
+add_action( 'show_user_profile', 'custom_user_profile_fields', 10, 1 );
+add_action( 'edit_user_profile', 'custom_user_profile_fields', 10, 1 );
+
+// 4. Saving the values.
+//add_action( 'personal_options_update', 'deptdir_save_local_avatar_fields' );
+add_action( 'edit_user_profile_update', 'deptdir_save_local_avatar_fields' );
+function deptdir_save_local_avatar_fields( $user_id ) {
+	if ( current_user_can( 'edit_user', $user_id ) ) {
+		if( isset($_POST[ 'deptdir-custom-avatar' ]) ){
+			$avatar = esc_url_raw( $_POST[ 'deptdir-custom-avatar' ] );
+			update_user_meta( $user_id, 'deptdir-custom-avatar', $avatar );
+		}
+	}
+}
+
+// 5. Set the uploaded image as default gravatar.
+add_filter( 'get_avatar_url', 'deptdir_get_avatar_url', 10, 3 );
+function deptdir_get_avatar_url( $url, $id_or_email, $args ) {
+	$id = '';
+	if ( is_numeric( $id_or_email ) ) {
+		$id = (int) $id_or_email;
+	} elseif ( is_object( $id_or_email ) ) {
+		if ( ! empty( $id_or_email->user_id ) ) {
+			$id = (int) $id_or_email->user_id;
+		}
+	} else {
+		$user = get_user_by( 'email', $id_or_email );
+		$id = !empty( $user ) ?  $user->data->ID : '';
+	}
+	//Preparing for the launch.
+	$custom_url = $id ?  get_user_meta( $id, 'deptdir-custom-avatar', true ) : '';
+	
+	// If there is no custom avatar set, return the normal one.
+	if( $custom_url == '' || !empty($args['force_default'])) {
+		return esc_url_raw( 'https://wpgd-jzgngzymm1v50s3e3fqotwtenpjxuqsmvkua.netdna-ssl.com/wp-content/uploads/sites/12/2020/07/blm-avatar.png' ); 
+	}else{
+		return esc_url_raw($custom_url);
+	}
+}
+
+
+
+
+
+
