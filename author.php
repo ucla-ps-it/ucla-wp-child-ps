8063f285511571de5dc169677a7b8014f3ce3f84
Yasmine Talby <57681204+yastalby@users.noreply.github.com>
2021-07-10 23:49:28 -0700

    Update author.php

--- author.php
+++ author.php
@@ -14,22 +14,21 @@
     </header>
 <hr/>
     <div class="ucla campus">
-<div class="col span_12_of_12">
+<div class="col span_9_of_12">
 <div id="author_picture">
 <?php echo get_avatar( get_the_author_meta('ID'), 200); ?>
 </div> </div>
-/bin/bash: wq: command not found
+<div class="col span_3_of_12">
 <div class="author-info">
 <p><b><?php echo get_the_author_meta('email',$author_id);?>
 <br>
 <?php echo get_the_author_meta('website', $author_id);?>
 <br>
 <?php echo get_the_author_meta('phone-number', $author_id);?>
-<br>
-<?php echo get_the_author_meta('office-location', $author_id);?>
+<br><?php echo get_the_author_meta('office-location', $author_id);?>
 <br></b></p>
 </div></div>
-      <div class="col span_9_of_12">
+      <div class="col span_12_of_12">
         </div>
 </div>
 </main>
